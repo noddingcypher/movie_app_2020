b98b41daa9da9faeb0cbe27fdf0b69db84e70531
noddingcypher <kangsj24@gmail.com>
2020-04-19 23:06:40 +0900

    Updates

---
+++ precache-manifest.ab2545e1dfd70888e80cb00f2cac0a6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d813984f9b5b3ff71c2f53669556d3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "dfd1f1f54cf3fe373da7",
+    "url": "/movie_app_2020/static/css/main.79fa9e65.chunk.css"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd1f1f54cf3fe373da7",
+    "url": "/movie_app_2020/static/js/main.4219ccbb.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
